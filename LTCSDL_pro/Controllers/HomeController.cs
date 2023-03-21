29bd52c4a41876679ad80a0fa00d6f9287cf4b37
phunglonghoang <91148361+phunglonghoang@users.noreply.github.com>
2023-03-22 04:00:41 +0700

    update

--- LTCSDL_pro/Controllers/HomeController.cs
+++ LTCSDL_pro/Controllers/HomeController.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LTCSDL_pro.Models;
 
 namespace LTCSDL_pro.Controllers
 {
     public class HomeController : Controller
     {
+        QuanLyBanSachEntities db = new QuanLyBanSachEntities();
         public ActionResult Index()
         {
             return View();
+        }
+        public PartialViewResult SachMoiPartial()
+        {
+            var lstSachMoi = db.Saches.Take(4).ToList();
+            return PartialView(lstSachMoi);
+
+        }
+        public PartialViewResult DetailsPartial()
+        {
+            var lstDetails = db.Saches.Take(1).ToList();
+            return PartialView(lstDetails);
+        }
+        public PartialViewResult HomePartial()
+        {
+            var lstHome = db.Saches.Take(4).ToList();
+            return PartialView(lstHome);
+        }
+        public PartialViewResult Main1Partial()
+        {
+            var lstMain1 = db.ChuDes.Take(8).ToList();
+            return PartialView(lstMain1);
+        }
+        public PartialViewResult Main2Partial()
+        {
+            var lstMain2 = db.TacGias.Take(8).ToList();
+            return PartialView(lstMain2);
         }
 
         public ActionResult About()
